28cb469f562f08aaba8b5ff84fd0ad6f6b944f32
kyawlinnthant <kyawlinnthant50@gmail.com>
2022-03-14 02:11:41 +0630

    Done Product page and add product section

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <h3>Kyaw Linn Thant</h3>
+        </div>
+    </div>
+@endsection
